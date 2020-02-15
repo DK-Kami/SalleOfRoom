515347a5d42078964110abab2df231072c371233
Kami San <1.dankon.1@gmail.com>
2020-02-15 09:38:17 +0300

    Компонент для верха страницы с CRUD'ом

---
+++ src/components/base/TopBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-layout class="px-7 pt-6" align-center>
+    <v-flex shrink class="title mr-6">
+      <slot name="title">{{ title }}</slot>
+    </v-flex>
+
+    <v-flex shrink v-if="withActions">
+      <slot name="actions">
+        <v-btn
+          color="primary"
+          fab
+          @click="action"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </slot>
+    </v-flex>
+    <v-spacer />
+
+    <v-flex xs3 v-if="withSearch">
+      <v-text-field
+        v-model="searchComp"
+        append-icon="mdi-magnify"
+        label="Поиск"
+        class="pt-0"
+        hide-details
+        single-line
+      />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+
+  props: {
+    withActions: {
+      type: Boolean,
+      default: true,
+    },
+    withSearch: {
+      type: Boolean,
+      default: true,
+    },
+    search: String,
+    title: String,
+  },
+
+  computed: {
+    searchComp: {
+      get() {
+        return this.search;
+      },
+      set(search) {
+        this.$emit('input', search);
+      },
+    },
+  },
+
+  methods: {
+    action() {
+      return this.$emit('action');
+    },
+  },
+};
+</script>
