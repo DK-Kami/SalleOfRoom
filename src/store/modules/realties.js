7aa70c3b23008e2e9bec6115e8e11a3118c735a3
Kami -san <1.dankon.1@gmail.com>
2020-03-19 09:20:11 +0300

    Задание корпуса

--- src/store/modules/realties.js
+++ src/store/modules/realties.js
@@ -74,7 +74,7 @@ export const mutations = {
 
   SET_REALTY: (state, realty) => {
     state.realty.priceArea         = parseFloat(realty.PriceArea).toFixed(2);
-    state.realtor.housingNumber    = realty.HousingNumber;
+    state.realty.housingNumber     = realty.HousingNumber;
 
     state.realty.сadastralNumber   = realty.CadastralNumber;
     state.realty.storeysNumber     = realty.StoreysNumber;
