cecbd311724bcdff28e51c16f44efe7983052819
Kami <1.dankon.1@gmail.com>
2020-04-10 23:23:47 +0300

    Слой для людей

---
+++ src/components/layouts/NonPrivateLayout/TheToolbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar color="white" class="toolbar" flat>
+    <template class="cursor--pointer">
+      <v-img src="/logo.png" max-width="60px" contain />
+      <span class="headline ml-3">Маяк</span>
+    </template>
+
+    <v-toolbar-items class="ml-6">
+      <v-btn
+        to="/sale/list"
+        class="body-2"
+        text
+      >Продажа</v-btn>
+
+      <v-btn
+        to="/buy/list"
+        class="body-2"
+        text
+      >Аренда</v-btn>
+
+      <v-btn
+        to="/services"
+        class="body-2"
+        text
+      >Услуги</v-btn>
+
+      <v-btn
+        to="/about"
+        class="body-2"
+        text
+      >О нас</v-btn>
+
+      <v-btn
+        to="/contacts"
+        class="body-2"
+        text
+      >Контакты</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheToolbar',
+};
+</script>
+
+<style>
+  .toolbar {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+  }
+</style>
--- src/components/layouts/NonPrivateLayout/index.vue
+++ src/components/layouts/NonPrivateLayout/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    yare yare daze
-  </div>
+  <v-content>
+    <the-toolbar />
+
+    <v-container class="pa-0" :class="$vuetify.breakpoint.xs ? 'mt-0' : 'mt-6'">
+      <v-layout fill-height justify-center>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
+import TheToolbar from './TheToolbar';
+
 export default {
   name: 'NonPrivatelayout',
 
-  // 
+  components: {
+    TheToolbar,
+  },
 };
 </script>
