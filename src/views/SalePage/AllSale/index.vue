e8770735c1593b523d4e55e3269c0004fed08bda
Kami <1.dankon.1@gmail.com>
2020-04-26 19:11:16 +0300

    Да, это самое

--- src/views/SalePage/AllSale/index.vue
+++ src/views/SalePage/AllSale/index.vue
@@ -8,14 +8,33 @@
       <v-flex xs6></v-flex>
     </v-layout>
 
-    <template v-if="view === 'list'">
-      <div class="headline font-weight-bold px-6">Продажа квартир в Домодедово</div>
-    </template>
+    <v-layout v-if="view === 'list'" class="px-6 py-4" column>
+      <div class="headline font-weight-bold">Продажа квартир в Домодедово</div>
+      <v-layout align-center>
+        <span class="subtitle mr-2">Сортировать по:</span>
+        <v-select
+          v-model="sortBy"
+          :items="sortTypes"
+          append-icon="mdi-chevron-down"
+          style="max-width: 105px"
+          hide-details
+          single-line
+          dense
+          flat
+          solo
+        />
+      </v-layout>
+    </v-layout>
   </v-layout>
 </template>
 
 <script>
 import SortBar from './SortBar';
+
+const sortTypes = [
+  { text: 'Дате', value: 'date'   },
+  { text: 'Цене', value: 'price'  },
+];
 
 export default {
   name: 'SalleAll',
@@ -25,7 +44,9 @@ export default {
   },
 
   data: () => ({
+    sortBy: 'date',
     view: 'list',
+    sortTypes,
   }),
 
   computed: ({
