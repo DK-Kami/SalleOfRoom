ff8c9f4bdc300d7cad89a0c066833fafb9cd1e49
Kami <1.dankon.1@gmail.com>
2020-05-07 12:15:29 +0300

    Отображение

--- src/views/Realties/RealtiesModify/ServicesForm.vue
+++ src/views/Realties/RealtiesModify/ServicesForm.vue
@@ -1,36 +1,36 @@
 <template>
   <v-container grid-list-xl px-0>
     <v-card class="px-6">
-      <v-layout>
-        <v-flex xs6>
+      <v-layout wrap>
+        <v-flex xs3>
           <v-checkbox
             v-model="realty.onDomClick"
             label="Выгрузка: ДомКлик"
           />
         </v-flex>
 
-        <v-flex xs6>
+        <v-flex xs3>
           <v-checkbox
             v-model="realty.OnRestat"
             label="Выгрузка: Рестат"
           />
         </v-flex>
 
-        <v-flex xs6>
+        <v-flex xs3>
           <v-checkbox
             v-model="realty.OnMirKvartir"
             label="Выгрузка: Мир квартир"
           />
         </v-flex>
 
-        <v-flex xs6>
+        <v-flex xs3>
           <v-checkbox
             v-model="realty.OnGdeEtotDom"
             label="Выгрузка: Где этот дом"
           />
         </v-flex>
 
-        <v-flex xs6>
+        <v-flex xs3>
         </v-flex>
       </v-layout>
     </v-card>
