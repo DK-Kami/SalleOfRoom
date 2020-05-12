456dda1e92b306b1191c79fe088fb99ae94ef372
Kami <1.dankon.1@gmail.com>
2020-05-12 15:08:44 +0300

    adaptive

--- src/views/Realties/RealtiesModify/ServicesForm.vue
+++ src/views/Realties/RealtiesModify/ServicesForm.vue
@@ -9,28 +9,28 @@
           />
         </v-flex>
 
-        <v-flex xs3>
+        <v-flex xs12 sm6 md3 lg3>
           <v-checkbox
             v-model="realty.OnRestat"
             label="Выгрузка: Рестат"
           />
         </v-flex>
 
-        <v-flex xs3>
+        <v-flex xs12 sm6 md3 lg3>
           <v-checkbox
             v-model="realty.OnMirKvartir"
             label="Выгрузка: Мир квартир"
           />
         </v-flex>
 
-        <v-flex xs3>
+        <v-flex xs12 sm6 md3 lg3>
           <v-checkbox
             v-model="realty.OnGdeEtotDom"
             label="Выгрузка: Где этот дом"
           />
         </v-flex>
 
-        <v-flex xs3>
+        <v-flex xs12 sm6 md3 lg3>
         </v-flex>
       </v-layout>
     </v-card>
